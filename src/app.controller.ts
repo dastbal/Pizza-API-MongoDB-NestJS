2861e302acb55d1bbc65857b315ec7aab347a65a
dastballadares <bal20001@byui.edu>
2022-02-07 02:28:00 -0500

    a

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -3,8 +3,8 @@ import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
-  @Get('')
+  constructor(private readonly appService: AppService) { }
+  @Get('/')
   home() {
     return this.appService.getHello();
   }
