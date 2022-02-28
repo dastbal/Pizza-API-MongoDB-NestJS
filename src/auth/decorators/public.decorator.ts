3b122b7e185894b0629173a5259ce95981dbfe0c
dastball <you@example.com>
2022-02-28 17:32:11 -0500

    finish I nned to implement roles

--- src/auth/decorators/public.decorator.ts
+++ src/auth/decorators/public.decorator.ts
@@ -2,4 +2,4 @@ import { Controller, Get, SetMetadata, UseGuards } from '@nestjs/common';
 
 export const IS_PUBLIC_KEY = 'isPublic';
 
-export  const Public = () => SetMetadata(IS_PUBLIC_KEY,true);
+export const Public = () => SetMetadata(IS_PUBLIC_KEY, true);
--- src/auth/strategies/jwt.strategy.ts
+++ src/auth/strategies/jwt.strategy.ts
@@ -7,14 +7,12 @@ import { PayloadToken } from '../models/token.model';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
-  constructor( @Inject(config.KEY) configService : ConfigType<typeof config>) {
-    super({ 
-        jwtFromRequest : ExtractJwt.fromAuthHeaderAsBearerToken(),
-        ignoreExpiration : false,
-        secretOrKey : configService.jwtSecret,
+  constructor(@Inject(config.KEY) configService: ConfigType<typeof config>) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: configService.jwtSecret,
     });
-
-
   }
 
   async validate(payload: PayloadToken) {
